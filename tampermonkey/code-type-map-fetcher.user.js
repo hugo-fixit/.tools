750f5d5ab94adfb37bdaca140d08dc8a05e70997
Cell <1024@lruihao.cn>
2022-08-29 14:39:33 +0800

    :sparkles: Feat: add tampermonkey script  for getting code-type-map from the list of Chroma highlighting languages

---
+++ tampermonkey/code-type-map-fetcher.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         code-type-map-fetcher
+// @namespace    https://github.com/hugo-fixit/.tools
+// @version      0.1
+// @description  Get code-type-map from the list of Chroma highlighting languages
+// @author       Lruihao (https://lruihao.cn)
+// @match        https://gohugo.io/content-management/syntax-highlighting/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=gohugo.io
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const container = document.querySelector('dl');
+    const codes = container.querySelectorAll('dd');
+    const types = container.querySelectorAll('dt');
+    const codeTypeMap = {};
+
+    // Fetch origin map and Split duplicate code
+    for (let i = 0; i < codes.length; i++) {
+        const code = codes[i].innerText.replace(/([+.#])/g,'\\$1');
+        const type = types[i].innerText;
+        if (!code) {
+            continue;
+        }
+        if (code.includes(', ')) {
+            const codeSplit = code.split(', ');
+            for (const c of codeSplit) {
+                codeTypeMap[`language-${c}`] = type;
+            }
+            continue;
+        }
+        codeTypeMap[`language-${code}`] = type;
+    }
+
+    // Print and cope to _maps/_code-type.scss
+    console.log(JSON.stringify(codeTypeMap));
+})();
